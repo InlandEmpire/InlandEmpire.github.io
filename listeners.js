2001bdcff342e19e77886ce60793b525416f29d7
Tristan Lundgreen <73362385+InlandEmpire@users.noreply.github.com>
2023-06-16 18:59:34 -0600

    Favicon added, images refined, text formatted
    Added favicon and image rounding in addition to drop shadow capabilities. Body text is now properly arranged and scales using a JS listener.

---
+++ listeners.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize', function() {
+    var sourceElement = document.getElementById('tagline');
+    var targetElementOne = document.getElementById('introduction_1');
+    var targetElementTwo = document.getElementById('introduction_2');
+    
+    var sourceWidth = sourceElement.offsetWidth;
+    targetElementOne.style.maxWidth = sourceWidth + 'px';
+    targetElementTwo.style.maxWidth = sourceWidth + 'px';
+  });
+  
